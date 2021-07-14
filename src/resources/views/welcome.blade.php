6e941d4e68376386a201af9eb418aa0ea90b3cc9
Jamie <htcjamiecox@gmail.com>
2021-07-15 02:59:25 +0900

    Created form and Classes for book information

--- src/resources/views/welcome.blade.php
+++ src/resources/views/welcome.blade.php
@@ -1,100 +1,180 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<?php
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+class Book
+{
+    public $title;
+    public $author;
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
+    public function __construct()
+    {
+        $this->title = $_GET['title'];
+        $this->author = $_GET['author'];
+    }
 
-            .full-height {
-                height: 100vh;
-            }
+    public function delete()
+    {
+    }
 
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
+    public function edit()
+    {
+    }
+}
 
-            .position-ref {
-                position: relative;
-            }
+class BookShelf
+{
 
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
+    public $books = array();
 
-            .content {
-                text-align: center;
-            }
+    public function add()
+    {
+        $book = new Book;
+        array_push($this->books, $book);
+        console_log($this->books);
+    }
 
-            .title {
-                font-size: 84px;
-            }
+    public function __construct()
+    {
+        $this->books = $this->books;
+    }
+}
 
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
+if (!isset($shelf)) {
+    console_log("IM GENERATED");
+    $shelf = new BookShelf;
+}
 
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
+
+function console_log($output, $with_script_tags = true)
+{
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) .
+        ');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+?>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Book Shelf</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+    <!-- Styles -->
+    <style>
+        html,
+        body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Nunito', sans-serif;
+            font-weight: 200;
+            height: 100vh;
+            margin: 0;
+        }
+
+        .full-height {
+            height: 100vh;
+        }
+
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+
+        .position-ref {
+            position: relative;
+        }
+
+        .top-right {
+            position: absolute;
+            right: 10px;
+            top: 18px;
+        }
+
+        .content {
+            text-align: center;
+        }
+
+        .title {
+            font-size: 84px;
+        }
+
+        .links>a {
+            color: #636b6f;
+            padding: 0 25px;
+            font-size: 13px;
+            font-weight: 600;
+            letter-spacing: .1rem;
+            text-decoration: none;
+            text-transform: uppercase;
+        }
+
+        .m-b-md {
+            margin-bottom: 30px;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a>
+            @else
+            <a href="{{ route('login') }}">Login</a>
+
+            @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a>
             @endif
+            @endauth
+        </div>
+        @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <div class="content">
+            <div class="title m-b-md">
+                Bookshelf
+            </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+            <div class="form">
+                <form method="GET">
+                    Title: <input type="text" name="title"><br>
+                    Author: <input type="text" name="author"><br>
+                    <input type="submit" value="Add" />
+                </form>
+            </div>
+
+            <?php
+            if (isset($_GET['title'])) {
+                $shelf->add();
+            }
+
+            ?>
+
+            <div class="booklist">
+                <table style="width: 100%">
+                    <tr>
+                        <th>Title</th>
+                        <th>Author</th>
+                    </tr>
+
+                    <?php foreach ($shelf->books as $book) : ?>
+                        <tr>
+                            <td><?php echo htmlspecialchars($book->title) ?> </td>
+                            <td><?php echo htmlspecialchars($book->author) ?> </td>
+                        <?php endforeach ?>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
