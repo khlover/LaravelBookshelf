ba5e88f1712264a348b83bede03e464ff278b271
Jamie <htcjamiecox@gmail.com>
2021-07-22 15:34:16 +0900

    Added PHPDoc comments across Book Controller.

--- src/app/Http/Controllers/BookController.php
+++ src/app/Http/Controllers/BookController.php
@@ -9,6 +9,13 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 
+/**
+ * BookController - Contains all methods related to using the Book Model
+ *
+ * @category Controllers
+ * @author   Jamie Cox <https://github.com/khlover>
+ * @package  BookController
+ */
 class BookController extends Controller
 {
     /**
@@ -17,16 +24,35 @@ class BookController extends Controller
      * @return void
      */
 
+    /**
+     * Returns data of one record from the database
+     *
+     * @param mixed $id - Chosen records bookId
+     *
+     * @return void
+     */
     function show($id)
     {
         return view('books.edit', ['book' =>  Book::find($id)]);
     }
 
+    /**
+     * Returns object of database table to the home view.
+     *
+     * @return void
+     */
     function index()
     {
         return view('books.home', ['books' => Book::sortable()->paginate(5)]);
     }
 
+    /**
+     * Searches database for author then returns an object from that filtered table.
+     *
+     * @param mixed $request - Contains both GET and POST values.
+     *
+     * @return void
+     */
     function searchAuthor(Request $request)
     {
         $author = $request->author;
@@ -34,6 +60,13 @@ class BookController extends Controller
         return view('books.home', ['books' => $books]);
     }
 
+    /**
+     * Searches database for title then returns an object from that filtered table.
+     *
+     * @param mixed $request - Contains both GET and POST values.
+     *
+     * @return void
+     */
     function searchTitle(Request $request)
     {
         $title = $request->title;
@@ -41,11 +74,24 @@ class BookController extends Controller
         return view('books.home', ['books' => $books]);
     }
 
+    /**
+     * Opens the create view.
+     *
+     * @return void
+     */
     public function create()
     {
         return view('books.create');
     }
 
+
+    /**
+     * Creates a new record based on request values then inserts it to the database.
+     *
+     * @param mixed $request - Contains both GET and POST values.
+     *
+     * @return void
+     */
     public function store(Request $request)
     {
         $title = $request->title;
@@ -59,6 +105,13 @@ class BookController extends Controller
         return redirect('/books');
     }
 
+    /**
+     * Updates record chosen by ID with the data in the request.
+     *
+     * @param mixed $request - Contains both GET and POST values.
+     *
+     * @return void
+     */
     public function editBook(Request $request)
     {
         $title = $request->title;
@@ -70,6 +123,13 @@ class BookController extends Controller
         return redirect('/books');
     }
 
+    /**
+     * Destroys record chosen by ID
+     *
+     * @param mixed $id - Chosen records bookId
+     *
+     * @return void
+     */
     public function destroy($id)
     {
         $target = Book::findOrFail($id);
@@ -77,6 +137,13 @@ class BookController extends Controller
         return redirect('/books');
     }
 
+    /**
+     * Sets what fields will be used to generate the CSV, then calls external function to perform the task.
+     *
+     * @param mixed $request - Contains both GET and POST values.
+     *
+     * @return void
+     */
     public function exportToCSV(Request $request)
     {
         $field = $request->field;
@@ -90,6 +157,13 @@ class BookController extends Controller
         Create_CSV($data, $field);
     }
 
+    /**
+     * Sets what fields will be used to generate the XML, then calls external function to perform the task.
+     *
+     * @param mixed $request - Contains both GET and POST values.
+     *
+     * @return void
+     */
     public function exportToXML(Request $request)
     {
         $field = $request->field;
@@ -104,6 +178,14 @@ class BookController extends Controller
     }
 }
 
+/**
+ * Creates XML file from database object then downloads to user via headers.
+ *
+ * @param mixed $obj   - Object containing filtered table data
+ * @param mixed $field - Table column field name
+ *
+ * @return void
+ */
 function Create_XML($obj, $field)
 {
     header('Content-type: text/xml');
@@ -137,6 +219,14 @@ function Create_XML($obj, $field)
     return redirect('/books');
 }
 
+/**
+ * Creates CSV file from the database object then downloads to user via headers.
+ *
+ * @param mixed $obj   - Object containing filtered table data
+ * @param mixed $field - Table column field name
+ *
+ * @return void
+ */
 function Create_CSV($obj, $field)
 {
     header("Content-Type: application/csv");
