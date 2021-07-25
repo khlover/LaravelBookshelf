5b98ea422a886003a7352cad1c1323c495c298ed
Jamie <htcjamiecox@gmail.com>
2021-07-26 00:16:13 +0900

    Added additonal phpunit tests
    - Alterted factory to generate random books using $faker.
    - Applied named routes to both search forms.
    - Changed route names to fit the books/ naming convention.
    - Created tests for
     -- Searching Titles and Authors
     -- Sorting by Titles and Authors ASC and DESC
     -- Pagination working correctly
     -- XML is correctly generated for Titles, Authors and Both.
     -- CSV is correctly generated for Titles, Authors and Both.

--- src/routes/web.php
+++ src/routes/web.php
@@ -20,11 +20,11 @@ Route::post('/books', [BookController::class, 'store'])->name('books.store');
 Route::post('/books/{id}', [BookController::class, 'editBook'])->name('books.edit');
 
 Route::get('/books/create', [BookController::class, 'create'])->name('books.create');
-Route::get('/export/csv/{search?}', [BookController::class, 'exportToCSV'])->name('books.export-csv');
-Route::get('/export/xml/{search?}', [BookController::class, 'exportToXML'])->name('books.export-xml');
+Route::get('/books/export/csv/{search?}', [BookController::class, 'exportToCSV'])->name('books.export-csv');
+Route::get('/books/export/xml/{search?}', [BookController::class, 'exportToXML'])->name('books.export-xml');
 Route::get('/books/{id}', [BookController::class, 'show'])->name('books.show');
-Route::get('/search/{author?}', [BookController::class, 'searchAuthor'])->name('books.search-author');
-Route::get('/search/title', [BookController::class, 'searchTitle'])->name('books.search-title');
+Route::get('/books/search/author', [BookController::class, 'searchAuthor'])->name('books.search-author');
+Route::get('/books/search/title', [BookController::class, 'searchTitle'])->name('books.search-title');
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 
 Route::delete('/books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
